e8077cf38886e954461503b2e7275c8129ae7281
Monique Gomes <niquevellin@gmail.com>
2026-09-10 16:20:20 -0300

    chore(infra): cria e configura Dockerfile e compose.yaml

--- agenda/agenda/settings.py
+++ agenda/agenda/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'agenda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'clinica_online'),
+        'USER': os.environ.get('POSTGRES_USER', 'clinica_admin'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'senha_super_segura_123'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'), # Usa 'db' quando rodar no Docker
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
@@ -126,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '*']
